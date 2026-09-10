0624f6e5a257c9b952c287a212cdbb0d9df0421b
Shivam Kumar <shivam.a@diggibyte.com>
2026-09-10 18:27:39 +0530

    Add SQL insert values example

---
+++ src/02_sql_basics/03_insert_values.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO Employees -- to insert value in the table
+(EmployeeID, EmployeeName, Age, Department, Salary)
+VALUES
+(102, 'Priya', 28, 'HR', 45000),
+(103, 'Amit', 24, 'IT', 55000),
+(104, 'Sneha', 30, 'Finance', 60000),
+(105, 'Vijay', 26, 'IT', 52000);
