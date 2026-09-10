f010777330a79a23a249cbab19982d9a2a41daeb
Shivam Kumar <shivam.a@diggibyte.com>
2026-09-10 18:28:00 +0530

    Add SQL alter table example

---
+++ src/02_sql_basics/10_alter_table.sql
@@ -0,0 +1,6 @@
+
+ALTER TABLE Employees -- used to update the structure of the table
+ADD Email VARCHAR(100); -- to add an column
+
+ALTER TABLE Employees
+ALTER COLUMN EmployeeName VARCHAR(100); -- to modify an existing column
