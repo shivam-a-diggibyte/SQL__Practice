4fbfc70c557a43fbf55af1bad49acca6c790547e
Shivam Kumar <shivam.a@diggibyte.com>
2026-09-10 18:27:33 +0530

    Add database creation SQL example

---
+++ src/02_sql_basics/01_create_database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE SQL_Training; -- to create a database
+GO -- is a batch separator commonly used by SQL Server tools such as SSMS
+
+USE SQL_Training; -- to use an existing database
+GO
+
+SELECT DB_NAME(); -- to  database
